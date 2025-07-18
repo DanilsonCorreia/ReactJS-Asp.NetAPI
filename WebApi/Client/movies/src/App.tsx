67cb780fdf9c3f91892d04acd29f6634e5e5bbe2
DanilsonCorreia <danilsoncorreia10@gmail.com>
2025-07-18 19:59:49 +0100

    react front end error handling with AxiosInterceptor

--- WebApi/Client/movies/src/App.tsx
+++ WebApi/Client/movies/src/App.tsx
@@ -2,9 +2,15 @@ import { Outlet, useLocation } from 'react-router-dom'
 import './App.css'
 import MovieTable from './components/movies/MovieTable'
 import { Container } from 'semantic-ui-react';
+import { useEffect } from 'react';
+import { setupErrorHandlingInterceptor } from './interceptors/axiosInterceptor';
 
 function App() {
     const location = useLocation();
+
+    useEffect(() => {
+        setupErrorHandlingInterceptor();
+    }, []);
 
   return (
       <>
--- WebApi/Client/movies/src/api/apiConnector.ts
+++ WebApi/Client/movies/src/api/apiConnector.ts
@@ -2,64 +2,48 @@
 import type { MovieDto } from "../models/movieDto";
 import axios from "axios";
 import type { GetMoviesResponse } from "../models/getMoviesResponse";
-import {API_BASE_URL} from "../../config.ts";
+import { API_BASE_URL } from "../../config.ts";
 import type { GetMovieByIdResponse } from "../models/getMovieByIdResponse.ts";
 
 const apiConnector = {
 
     getMovies: async (): Promise<MovieDto[]> => {
-        try {
-            const response: AxiosResponse<GetMoviesResponse> =
-                await axios.get(`${API_BASE_URL }/movies`);
-            const movies = response.data.movieDtos.map(movie => ({
-                ...movie,
-                createDate: movie.createDate?.slice(0, 10) ?? ""
-            }));
-            return movies;
-        }
-        catch (error) {
-            console.log('Error fetching movies:', error);
-            throw error;
-        }
+
+        const response: AxiosResponse<GetMoviesResponse> =
+            await axios.get(`${API_BASE_URL}/movies`);
+        const movies = response.data.movieDtos.map(movie => ({
+            ...movie,
+            createDate: movie.createDate?.slice(0, 10) ?? ""
+        }));
+        return movies;
+
     },
 
     createMovie: async (movie: MovieDto): Promise<void> => {
-        try {
-            await axios.post<number>(`${API_BASE_URL}/movies`, movie)
-        } catch (error) {
-            console.log(error);
-            throw error;
-        }
+
+        await axios.post<number>(`${API_BASE_URL}/movies`, movie)
+
     },
 
     editMovie: async (movie: MovieDto): Promise<void> => {
-        try {
-            await axios.put<number>(`${API_BASE_URL}/movies/${movie.id}`, movie)
-        } catch (error) {
-            console.log(error);
-            throw error;
-        }
+
+        await axios.put<number>(`${API_BASE_URL}/movies/${movie.id}`, movie)
+
     },
 
     deleteMovie: async (movieId: number): Promise<void> => {
-        try {
-            await axios.delete<number>(`${API_BASE_URL}/movies/${movieId}`)
-        } catch (error) {
-            console.log(error);
-            throw error;
-        }
+
+        await axios.delete<number>(`${API_BASE_URL}/movies/${movieId}`)
+
     },
 
     getMovieById: async (movieId: string): Promise<MovieDto | undefined> => {
-        try {
-            const response = await axios
-                .get<GetMovieByIdResponse>(`${API_BASE_URL}/movies/${movieId}`);
-            return response.data.movieDto;
-        } catch (error) {
-            console.log(error);
-            throw error;
-        }
-        
+
+        const response = await axios
+            .get<GetMovieByIdResponse>(`${API_BASE_URL}/movies/${movieId}`);
+        return response.data.movieDto;
+
+
     }
 
 }
